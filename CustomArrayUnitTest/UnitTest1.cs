9c4022d97a206001226acdd94f24aacc9909e9f3
tim.rosenbach <tim.rosenbach@brickmakers.de>
2024-08-15 09:32:12 +0200

    init

---
+++ CustomArrayUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CustomArrayUnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
