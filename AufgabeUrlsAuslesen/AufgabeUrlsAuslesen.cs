9e22e4a461772fb12a4e2345c5d7835d7004cd36
Felix Langstengel <felix.langstengel@brickmakers.de>
2024-08-15 16:57:19 +0200

    Read from Http

--- AufgabeUrlsAuslesen/AufgabeUrlsAuslesen.cs
+++ AufgabeUrlsAuslesen/AufgabeUrlsAuslesen.cs
@@ -1,11 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace AufgabeUrlsAuslesen;
+using System.Net.Http.Headers;
 
-internal class AufgabeUrlsAuslesen
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    var json = await client.GetStringAsync("https://learn.microsoft.com");
+    Console.WriteLine(json);
 }
