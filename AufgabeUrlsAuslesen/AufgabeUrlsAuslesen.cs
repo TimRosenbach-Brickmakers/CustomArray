1589f3a5d2d12cd95fda9ca81a9f9b36572c9c4b
Felix Langstengel <felix.langstengel@brickmakers.de>
2024-08-15 15:48:30 +0200

    Custom Array Programm

---
+++ AufgabeUrlsAuslesen/AufgabeUrlsAuslesen.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace AufgabeUrlsAuslesen;
+
+internal class AufgabeUrlsAuslesen
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
