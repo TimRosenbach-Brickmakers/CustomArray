ac371bae9a87d843836fb14ca721dfdf721e6080
Felix Langstengel <felix.langstengel@brickmakers.de>
2024-08-16 08:25:05 +0200

    Added the Url Array and print statement

--- AufgabeUrlsAuslesen/AufgabeUrlsAuslesen.cs
+++ AufgabeUrlsAuslesen/AufgabeUrlsAuslesen.cs
@@ -8,10 +8,39 @@ client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-await ProcessRepositoriesAsync(client);
-
-static async Task ProcessRepositoriesAsync(HttpClient client)
+var urls = new string[]
 {
-    var json = await client.GetStringAsync("https://learn.microsoft.com");
-    Console.WriteLine(json);
+    "https://learn.microsoft.com",
+    "https://learn.microsoft.com/aspnet/core",
+    "https://learn.microsoft.com/azure",
+    "https://learn.microsoft.com/azure/devops",
+    "https://learn.microsoft.com/dotnet",
+    "https://learn.microsoft.com/dotnet/desktop/wpf/get-started/create-app-visual-studio",
+    "https://learn.microsoft.com/education",
+    "https://learn.microsoft.com/shows/net-core-101/what-is-net",
+    "https://learn.microsoft.com/enterprise-mobility-security",
+    "https://learn.microsoft.com/gaming",
+    "https://learn.microsoft.com/graph",
+    "https://learn.microsoft.com/microsoft-365",
+    "https://learn.microsoft.com/office",
+    "https://learn.microsoft.com/powershell",
+    "https://learn.microsoft.com/sql",
+    "https://learn.microsoft.com/surface",
+    "https://dotnetfoundation.org",
+    "https://learn.microsoft.com/visualstudio",
+    "https://learn.microsoft.com/windows",
+    "https://learn.microsoft.com/maui"
+};
+
+await ProcessRepositoriesAsync(client, urls);
+
+static async Task ProcessRepositoriesAsync(HttpClient client, string[] urls)
+{
+    foreach (var str in urls)
+    {
+        var json = await client.GetStringAsync(str);
+        Console.WriteLine(json);
+    }
 }
+
+Console.WriteLine("Hello");
